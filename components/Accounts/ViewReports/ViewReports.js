ddb25c52b8e801f3c79dae6cebb2398cfa540a1f
Thu88 <jublen@protonmail.com>
2021-11-29 19:38:49 +0100

    Add pie chart to report section

--- components/Accounts/ViewReports/ViewReports.js
+++ components/Accounts/ViewReports/ViewReports.js
@@ -1,8 +1,64 @@
+import React from "react";
+import { Box, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 import PieChart from "../../PieChart";
+import { useEffect } from "react";
+import { useSession} from 'next-auth/client';
 
 const ViewReports = () => {
+    const [account, setAccount] = React.useState('');
+    const [accounts, setAccounts] = React.useState([]);
+    const [rows, setRows] = React.useState([]);
+    const [session] = useSession();
+    const user = session.user;
+
+    const handleChange = (event, child) => {
+        const id = Number(child.props.accountindex);
+        const accountNo = event.target.value;
+        console.log(event.target.value)
+        setAccount(event.target.value);
+  
+        fetch('/api/getaccountscontent', {
+          method: 'POST',
+          body: JSON.stringify({user, accountNo, id})
+        })
+        .then(res => res.json())
+        .then(res => {
+          console.log(res)
+          setRows(res.rows)
+        })
+      };
+
+      useEffect(() => {
+        fetch('/api/getaccounts', {
+          method: 'POST',
+          body: JSON.stringify({user: user})
+        }).then(res => res.json()).then(res =>{
+          if (res.message === 'success') {
+            const newAccounts = res.accounts;
+            setAccounts(newAccounts);
+          }
+        });
+
+      }, [])
+  
     return (
-        <PieChart />
+        <Box>
+            <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Account</InputLabel>
+                <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={account}
+                label="Account"
+                onChange={handleChange}
+                >
+                    {accounts.map((acc, index) => {
+                        return (<MenuItem accountindex={`${index}`} key={`${acc}:${index}`} value={acc}>{acc}</MenuItem>);
+                    })}
+                </Select>
+            </FormControl>
+            <PieChart rows={rows} />
+        </Box>
     );
 }
 
--- components/PieChart/PieChart.js
+++ components/PieChart/PieChart.js
@@ -1,11 +1,82 @@
 import { Box } from '@mui/material';
 import { Chart, registerables } from 'chart.js'
 import { useEffect, useRef } from 'react';
+import React from 'react';
+let pieChart;
 
-const PieChart = () => {
+function generateRandomColor(){
+    let maxVal = 0xFFFFFF; // 16777215
+    let randomNumber = Math.random() * maxVal; 
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    let randColor = randomNumber.padStart(6, 0);   
+    return `#${randColor.toUpperCase()}`
+}
+
+const PieChart = ({ rows }) => {
+    const [expenses, setExpenses] = React.useState([]);
+    const [categories, setCategories] = React.useState([]);
+    const [amounts, setAmounts] = React.useState([])
     const pieRef = useRef();
     Chart.register(...registerables)
-    let pieChart;
+    if (rows.toString() !== expenses.toString()) {
+        setExpenses(rows);
+        const categoryExpenses = rows.map(row => {
+            return {category: row.category, amount: Number(row.amount)};
+        });
+        console.log("categoryExpenses",categoryExpenses)
+        const categoryExpensesFormated = [];
+    
+        for (let row of categoryExpenses) {
+            if(categoryExpensesFormated.length === 0) {
+                categoryExpensesFormated.push(row);
+            }
+            categoryExpensesFormated.forEach((elem) => {
+                if (row.category === elem.category) {
+                    elem.amount += row.amount;
+                }
+                else {
+                    categoryExpensesFormated.push(row);
+                }
+            })
+        }
+        console.log("categoryExpensesFormated",categoryExpensesFormated)
+
+        const tempCategories = categoryExpensesFormated.map((elem) => {
+            return elem.category;
+        });
+
+        const tempAmounts = categoryExpensesFormated.map((elem) => {
+            return elem.amount;
+        });
+
+        console.log("tempCategories", tempCategories)
+        console.log("tempAmounts", tempAmounts)
+
+    
+        setCategories(tempCategories);
+        setAmounts(tempAmounts);
+
+        
+        console.log("categories", categories)
+        console.log("amounts", amounts)
+
+
+
+        pieChart.data.datasets[0].data = tempAmounts;
+        pieChart.data.labels = tempCategories;
+        pieChart.data.datasets[0].backgroundColor = tempCategories.map((elem, index) => {
+            const color = generateRandomColor();
+            console.log(color)
+            return color;
+        })
+
+        console.log(pieChart)
+        pieChart.update()
+
+    }
+    
+    
     useEffect(() => {
         const myChartRef = pieRef.current.getContext("2d");
         myChartRef.getContextimageSmoothingEnabled = true
@@ -14,18 +85,19 @@ const PieChart = () => {
             type: "pie",
             data: {
                 //Bring in data
-                labels: ["Jan", "Feb", "March"],
+                labels: categories,
                 datasets: [
                     {
                         label: "Sales",
-                        data: [86, 67, 91],
+                        data: amounts,
                     }
                 ]
             },
             options: {
-                //Customize chart options
+                
             }
         });
+        
         return () => {
            
         };
