d28d30075881a7843c4d6e4f6af1943d39fd561d
Thu88 <jublen@protonmail.com>
2021-12-03 15:04:30 +0100

    Next auth horoku fix #1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_SERVER} = require('next/constants');
 
 module.exports = (phase, { defaultConfig }) => {
-  let url = 'https://personance.herokuapp.com';
+  let url = 'https://personance.herokuapp.com/api/auth';
 
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     url = 'http://localhost:3000';
