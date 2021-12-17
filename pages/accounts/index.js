07cafe5006107c9577acc96440741fe6eaf8406b
Thu88 <jublen@protonmail.com>
2021-12-17 16:20:46 +0100

    Refactor accounts page

--- pages/accounts/index.js
+++ pages/accounts/index.js
@@ -5,18 +5,19 @@ import EditAccount from '../../components/Accounts/EditAccount';
 import ViewReports from '../../components/Accounts/ViewReports';
 import Budget from '../../components/Accounts/Budget';
 import { UPDATE_SUBMENU } from '../../redux/actionTypes';
-import { useSession, getSession} from 'next-auth/client';
+import {getSession} from 'next-auth/client';
 import { useRouter } from 'next/router';
 
 const Accounts = () => {
     const dispatch = useDispatch();
-    const {submenu} = useSelector(state => state.submenu);
+    const {submenu} = useSelector(state => state.submenu); //Subscribe to submenu state from Redux
     const router = useRouter();
     let session;
     
     let activeSubmenuEntry;
 
     useEffect(() => {
+        //When user clicks on Accounts menu button, give the submenu state from Redux its default values
         dispatch({
             type: UPDATE_SUBMENU,
             payload: {
@@ -40,10 +41,16 @@ const Accounts = () => {
                 ]
             }
         });
+
         getUser();
+        
         async function getUser() {
-            session = await getSession();
-            if (!session) {
+        /*  This function gets the current user session. If there is no session then no 
+            user has logged in, and the user is then redirected to the signin page. */
+
+            session = await getSession(); //Get session
+
+            if (!session) { //Redirect if no session availible
                 router.push({
                     pathname: '/signin',
                     query: {
@@ -54,6 +61,8 @@ const Accounts = () => {
         }
     }, []);
     
+    //Checking if the user has clicked on one of the submeny buttons. 
+    //If thats the case, the component representing the active submenu is saved
     if (true) {
         submenu.forEach((elem) => {
             if (elem.active === true) {
@@ -73,19 +82,12 @@ const Accounts = () => {
                 }
             }
         });
-    } else {
-        /* router.push({
-            pathname: '/signin',
-            query: {
-                text: true,
-            }
-        }); */
     }
     
 
     return (
         <>
-            {activeSubmenuEntry}
+            {activeSubmenuEntry} {/* Return the saved component representing the active submenu item */}
         </>
     );
 };
