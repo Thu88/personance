dc844d93b748c038cfbe5b9794104af1d85187e0
Thu88 <jublen@protonmail.com>
2021-12-17 17:03:42 +0100

    Refactor the nextauth configurations

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,45 +1,57 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import GithubProvider from "next-auth/providers/github";
 import { MongoClient } from 'mongodb';
 
 export default NextAuth({
     providers: [
         CredentialsProvider({
+            /* Use a credentials provider. A credentials provider is a custom provider made by the programmer.
+               It is also possible to use providers such as fx google and facebook. This way a user can login with their google
+               or facebook account  */
             name: 'Credentials',
             credentials: {      
                 username: { label: "Username", type: "text", placeholder: "jsmith" },      
                 password: {  label: "Password", type: "password" }    
             },
             async authorize(credentials, req) {
+                /* This function is run when the user tries to log in */
+
+                //Setup MongoClient
                 const uri = "mongodb+srv://thomasRoot:thmdikmfs89987IJBHIHBIU@personance.c4kx6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
                 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-                let test1;
+                
+                let user;
                 await client.connect();
                 const collection = client.db("personance").collection("users");
-                test1 = await collection.findOne({
+
+                //Check if the credenital provided matches a user in the database
+                user = await collection.findOne({
                     username: credentials.username,
                     password: credentials.password,
 
                 });                
-                console.log(test1)
+                console.log(user)
 
-                return test1;
+                //Return the user which then becomes availible in the session
+                return user;
             }
         })
     ],
     pages: {
+        //Use a custom sign in page instead of the default one provided by NextAuth
         signIn: '/signin'
     },
     callbacks: {
-           async redirect({ url, baseUrl }) {     
+            async redirect({ url, baseUrl }) {     
                 return url 
             },
             async session(session, token) {
-                //session.accessToken = token.accessToken;
+                //Make the user object availible in the session
                 session.user = token.user;
                 return session;
             },
+            //Configure the JSON Web Token to contain a user object with a username and password.
+            //By default NextAuth only saves the username and email.
             async jwt(token, user, account, profile, isNewUser) {
                 console.log("JWT User");
                 console.log(user);
