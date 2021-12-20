097b2324c46e371c70fa36bfa8475e8ebacebd11
Thu88 <jublen@protonmail.com>
2021-12-20 12:03:29 +0100

    Refactor SubMenu

--- components/SubMenu/SubMenu.js
+++ components/SubMenu/SubMenu.js
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux';
-import React, { useState } from 'react';
+import React from 'react';
 import { Paper, Box, Button, Typography} from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { UPDATE_SUBMENU } from '../../redux/actionTypes';
@@ -22,14 +22,20 @@ const SubMenu = ({ className }) => {
     const dispatch = useDispatch();
 
     const sendDispatch = (entryName) => {
+        /* This function activates the submenu entry when it is called */
+
+        //Find the index of the submenu entry in the submenu state in Redux
         const entryIndex = submenu.findIndex(entry => entry.name === entryName);
         
+        //Reset all other submenu items
         submenu.forEach((entry) => {
             entry.active = false;
         });
 
+        //Make the clicked submenu item active
         submenu[entryIndex].active = true;
         
+        //Update the submenu state in Redux
         dispatch({
             type: UPDATE_SUBMENU,
             payload: {
@@ -38,7 +44,6 @@ const SubMenu = ({ className }) => {
         });
     };
 
-    console.log(submenu)
     return (
         <Box className={className}>
             <Paper className={`${className} ${classes.paper}`} position="sticky">
