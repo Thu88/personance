48484ebe2c070f2ec381719457bc131153a5c392
Thu88 <jublen@protonmail.com>
2021-12-29 18:20:46 +0100

    Remove menu items from main menu

--- components/MainMenu/MainMenu.js
+++ components/MainMenu/MainMenu.js
@@ -17,9 +17,6 @@ function MainMenu( {className} ) {
     return (
         <Box className={className}>
             <Button href="/accounts" className={classes.button} color="secondary" variant="contained"><Typography>Accounts</Typography></Button>
-            <Button className={classes.button} color="secondary" variant="contained"><Typography>2</Typography></Button>
-            <Button className={classes.button} color="secondary" variant="contained"><Typography>3</Typography></Button>
-            <Button className={classes.button} color="secondary" variant="contained"><Typography>4</Typography></Button>
         </Box>
     );
 }
